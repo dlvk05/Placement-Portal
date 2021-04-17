44f4e093e877e97d8dfe8eaea81251adf1c8f551
Suvansh Shukla <suvanshshukla@gmail.com>
2021-04-17 13:24:46 +0530

    checked bootstrap working or not

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 class App extends React.Component{
   render() {
     return (
       <div>
         <h1>Can you see this??</h1>
+        <Button>Hello</Button>
       </div>
     );
   }
