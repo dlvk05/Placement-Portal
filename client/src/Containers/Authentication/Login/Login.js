e3ae21ae0ed46896b29775c4c2753c254aaf3681
dlvk05 <darklordvibhor@gmail.com>
2021-04-18 11:36:04 +0530

    confirmed input working

--- client/src/Containers/Authentication/Login/Login.js
+++ client/src/Containers/Authentication/Login/Login.js
@@ -3,51 +3,48 @@ import { Form, Button } from "react-bootstrap";
 import "./Login.css";
 
 class Login extends React.Component {
-    state ={
-        email:{
-            type:'email',
-            value:''
-        },
-        password:{
-            type:'password',
-            value:''
-        }
-    }
+  state = {
+    email: {
+      type: "email",
+      value: "",
+    },
+    password: {
+      type: "password",
+      value: "",
+    },
+  };
 
-    inputChangeHndler = (event, string) =>{
-        const input = event.target.value;
-        console.log(input);
-        const updatedState = {
-            ...this.state,
-            [string]:{
-                type:[string],
-                value: input
-            }
-        }
-        this.setState(updatedState)
-        console.log(this.state)
-    }
-
-  inputChangeHandler = (type, event) => {
+  inputChangeHandler = (event, string) => {
     const input = event.target.value;
+    console.log(input);
     const updatedState = {
       ...this.state,
-      [type]: {
-        type: [type],
+      [string]: {
+        type: [string],
         value: input,
       },
     };
-    this.setState(updatedState);
+     this.setState({ ...updatedState });
     console.log(this.state);
   };
 
+  random = () => {
+    console.log("from button");
+    console.log(this.state);
+  };
   render() {
     return (
       <div className="section">
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={(event, string) => this.inputChangeHndler(event, "email")}/>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(event, string) =>
+                this.inputChangeHandler(event, "email")
+              }
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -55,12 +52,20 @@ class Login extends React.Component {
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" onChange={(event, string) => this.inputChangeHndler(event, "password")}/>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(event, string) =>
+                this.inputChangeHandler(event, "password")
+              }
+            />
           </Form.Group>
           <Form.Group controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
-          <Button variant="primary">Submit</Button>
+          <Button variant="primary" onClick={this.random}>
+            Submit
+          </Button>
         </Form>
       </div>
     );
