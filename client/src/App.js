defeb817835343750e60b1d6b021410c10748825
Suvansh Shukla <suvanshshukla@gmail.com>
2021-04-18 10:58:41 +0530

    Login.js added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import { BrowserRouter, Route} from "react-router-dom";
+import Login from './Containers/Authentication/Login/Login';
+
 
 class App extends React.Component{
   render() {
     return (
-      <div>
-        <h1>Can you see this??</h1>
-        <Button>Hello</Button>
-      </div>
+      <BrowserRouter>
+        <Route path="/Login" exact component={Login}/>
+      </BrowserRouter>
     );
   }
 }
---
+++ client/src/Containers/Authentication/Login/Login.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {Form, Button} from 'react-bootstrap';
+import './Login.css'
+
+class Login extends React.Component {
+    state ={
+        email:{
+            type:'email',
+            value:''
+        },
+        password:{
+            type:'password',
+            value:''
+        }
+    }
+
+    inputChangeHndler = (type, event) =>{
+        const input = event.target.value;
+        const updatedState = {
+            ...this.state,
+            [type]:{
+                type:[type],
+                value: input
+            }
+        }
+        this.setState(updatedState)
+        console.log(this.state)
+    }
+
+
+  render() {
+    return (
+      <div className="section">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={("email", event) => this.inputChangeHndler("email", event)}/>
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Login;
