579d58e2cdd8e5e087fb141073e40200f569e8f1
Suvansh Shukla <suvanshshukla@gmail.com>
2021-04-19 11:26:36 +0530

    added fontawesome CDN and also imported user-circle icon and added bare minium styling

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Nav, Navbar} from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 const navbar = () => {
   return (
     <div>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">PLacement Portal</Navbar.Brand>
+        <Navbar.Brand href="#home">Placement Portal</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#JobProfiles">Job Profiles</Nav.Link>
+          <Nav.Link href="#PersonalInfo">Personal Info</Nav.Link>
         </Nav>
+        <div style={{ color: "white", marginRight: "50px" }}>
+          <i class="far fa-user-circle fa-2x"></i>
+        </div>
       </Navbar>
     </div>
   );
