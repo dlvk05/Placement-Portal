0bbe9bc80e39840e9565707721915c9e00f36867
Suvansh Shukla <suvanshshukla@gmail.com>
2021-04-25 22:19:31 +0530

    added education subfield for scores and added sem wise score table also made the modal for editing the same

--- client/src/Containers/Profile/Education/Current/CurrentModal.js
+++ client/src/Containers/Profile/Education/Current/CurrentModal.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Modal, Button, Form, Col } from "react-bootstrap";
+import {
+  Modal,
+  Button,
+  Form,
+  Col,
+  InputGroup,
+  Row,
+  Table,
+} from "react-bootstrap";
 
 class CurrentModal extends React.Component {
   state = {
@@ -11,79 +19,264 @@ class CurrentModal extends React.Component {
   };
 
   handleShow = () => {
-      this.setState({
-          show: !this.state.show
-      })
-  }
-
+    this.setState({
+      show: !this.state.show,
+    });
+  };
 
   render() {
     return (
       <div>
         <a href="#empty" onClick={this.handleShow}>
-        <i class="fas fa-edit"> Edit</i>
+          <i class="fas fa-edit"> Edit</i>
         </a>
 
-        <Modal show={this.state.show} onHide={this.handleShow}>
+        <Modal show={this.state.show} onHide={this.handleShow} size="lg">
           <Modal.Header closeButton>
             <Modal.Title>Edit Current Education Details</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form>
               <Form.Row>
-                <Form.Group as={Col} controlId="formGridCompleteAddress">
-                <Form.Label>Complete Address</Form.Label>
-                <Form.Control
-                  type="CompleteAddress"
-                  placeholder="Enter CompleteAddress"
-                  required
-                  onChange={(event, string) => {
-                    this.inputChangeHandler(event, "CompleteAddress");
-                  }}
-                />
+                <Form.Group controlId="exampleForm.SelectCustom">
+                  <Form.Label>Current Semester</Form.Label>
+                  <Form.Control
+                    as="select"
+                    custom
+                    /* onChange={(event, string) => {
+                  this.inputChangeHandler(event, "semester");
+                  console.log("drop down is being read");
+                }} */
+                  >
+                    <option eventkey="1">1</option>
+                    <option eventkey="2">2</option>
+                    <option eventkey="3">3</option>
+                    <option eventkey="4">4</option>
+                    <option eventkey="5">5</option>
+                    <option eventkey="6">6</option>
+                    <option eventkey="7">7</option>
+                    <option eventkey="8">8</option>
+                  </Form.Control>
                 </Form.Group>
               </Form.Row>
 
               <Form.Row>
-                <Form.Group as={Col} controlId="formGridPincode">
-                <Form.Label>Pincode</Form.Label> <br/>
-                <Form.Control
-                  type="Pincode"
-                  placeholder="Enter Pincode"
-                  required
-                  onChange={(event, string) => {
-                    this.inputChangeHandler(event, "Pincode");
-                  }}
-                />
-                </Form.Group>
+                <Row>
+                  <Col>
+                    <label htmlFor="">Score</label>
+                    <InputGroup className="mb-3">
+                      <Form.Control />
+                      <InputGroup.Append>
+                        <InputGroup.Text id="basic-addon2">
+                          CGPA
+                        </InputGroup.Text>
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Col>
+                  <Col>
+                    <label htmlFor="">Percentage Equivalent</label>
+                    <InputGroup className="mb-3">
+                      <Form.Control />
+                      <InputGroup.Append>
+                        <InputGroup.Text id="basic-addon2">%</InputGroup.Text>
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Col>
+                </Row>
               </Form.Row>
 
               <Form.Row>
-                <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label> <br/>
-                <Form.Control
-                  type="State"
-                  placeholder="Enter State"
-                  required
-                  onChange={(event, string) => {
-                    this.inputChangeHandler(event, "State");
-                  }}
-                />
-                </Form.Group>
+                <Row>
+                  <Col>
+                    <label htmlFor="">Course Start Date</label>
+                    <InputGroup className="mb-3">
+                      <InputGroup.Append>
+                        <Form.Control
+                          type="date"
+                          placeholder="Enter DOB"
+                          required
+                          onChange={(event, string) => {
+                            this.inputChangeHandler(event, "DOB");
+                          }}
+                        />
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Col>
+                  <Col>
+                    <label htmlFor="">Course End Date</label>
+                    <InputGroup className="mb-3">
+                      <InputGroup.Append>
+                        <Form.Control
+                          type="date"
+                          placeholder="Enter DOB"
+                          required
+                          onChange={(event, string) => {
+                            this.inputChangeHandler(event, "DOB");
+                          }}
+                        />
+                      </InputGroup.Append>
+                    </InputGroup>
+                  </Col>
+                </Row>
               </Form.Row>
 
               <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label> <br/>
-                <Form.Control
-                  type="City"
-                  placeholder="Enter City"
-                  required
-                  onChange={(event, string) => {
-                    this.inputChangeHandler(event, "City");
-                  }}
-                />
-                </Form.Group>
+                <Row>
+                  <Table size="sm">
+                    <Col>
+                      <thead>
+                        <tr>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col>CGPA</Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col>SGPA</Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+
+                          <th>
+                            <Col>Total</Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+                          <th>
+                            <Col></Col>
+                          </th>
+
+                          <th>
+                            <Col>Ongoing</Col>
+                          </th>
+                        </tr>
+                      </thead>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 1</td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 2</td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 3</td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 4</td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 5</td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                    <Col>
+                      <tr>
+                        <td>Semester 6 </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                        <td>
+                          <Form.Control size="sm" />
+                        </td>
+                      </tr>
+                    </Col>
+                  </Table>
+                </Row>
               </Form.Row>
             </Form>
           </Modal.Body>
--- client/src/Containers/Profile/Education/Education.js
+++ client/src/Containers/Profile/Education/Education.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Table } from "react-bootstrap";
 import CurrentModal from "./Current/CurrentModal";
 
 const styles = {
@@ -41,7 +41,58 @@ class Education extends React.Component {
         </Row>
         <hr />
         <br />
-        
+        <Row>
+          <Col xs="2">Semester wise scores</Col>
+        </Row>
+        <Table>
+          <thead>
+            <tr>
+              <th>Semester</th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+            </tr>
+          </thead>
+          <tr>
+            <td>CGPA</td>
+            <td>num1</td>
+            <td>num2</td>
+            <td>num3</td>
+            <td>num4</td>
+            <td>num5</td>
+            <td>num6</td>
+          </tr>
+          <tr>
+            <td>SGPA</td>
+            <td>num1</td>
+            <td>num2</td>
+            <td>num3</td>
+            <td>num4</td>
+            <td>num5</td>
+            <td>num6</td>
+          </tr>
+          <tr>
+            <td>Ongoing Backlogs</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+          </tr>
+          <tr>
+            <td>Total Backlogs</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+            <td>num</td>
+          </tr>
+        </Table>
       </div>
     );
   }
