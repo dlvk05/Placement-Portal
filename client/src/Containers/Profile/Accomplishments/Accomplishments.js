d042607a0e46d081d29b3ad604d58a00d7a49cac
Suvansh Shukla <suvanshshukla@gmail.com>
2021-04-30 23:13:51 +0530

    minor changes to accomplishments section

--- client/src/Containers/Profile/Accomplishments/Accomplishments.js
+++ client/src/Containers/Profile/Accomplishments/Accomplishments.js
@@ -32,14 +32,20 @@ class Accomplishments extends React.Component {
             </span>
           </Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Issuer:</span></Col>
             <Col><span>"Issuer Name"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Issue Date</span></Col>
             <Col><span>"date goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"Description goes here"</span></Col>
@@ -63,22 +69,32 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"Certification Title goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Issuer</span></Col>
             <Col><span>"Issuer name goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Certification URL</span></Col>
             <Col><span>"Issuer Certification URL goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Certification Date</span></Col>
             <Col><span>"Certification Date goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Licence Number</span></Col>
             <Col><span>"Licence Number goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"Description goes here"</span></Col>
@@ -102,14 +118,20 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"title Goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Position</span></Col>
             <Col><span>"position goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Competition Date</span></Col>
             <Col><span>"Competition Date goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"Description goes here"</span></Col>
@@ -133,14 +155,20 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"Title goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Organizer</span></Col>
             <Col><span>"Organizer info goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Event Date</span></Col>
             <Col><span>"Event Date goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"Description goes here"</span></Col>
@@ -164,22 +192,32 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"Title goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Score Obtained</span></Col>
             <Col><span>"score obtained goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Maximum Possible Score</span></Col>
             <Col><span>"max possible score goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Rank Obtained</span></Col>
             <Col><span>"Rank obtained goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Exam Date</span></Col>
             <Col><span>"exam date goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"Description goes here"</span></Col>
@@ -203,18 +241,26 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"title goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Publisher</span></Col>
             <Col><span>"Publisher Info goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Publication Date</span></Col>
             <Col><span>"publication date goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Publication URL</span></Col>
             <Col><span>"publication url goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"description goes here"</span></Col>
@@ -238,14 +284,20 @@ class Accomplishments extends React.Component {
             <Col xs={2}><span>Title</span></Col>
             <Col><span>"title goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Grant Date</span></Col>
             <Col><span>"Grant date goes here</span></Col>
         </Row>
+        <hr />
+        <br />
         <Row>
             <Col xs={2}><span>Description</span></Col>
             <Col><span>"description goes here"</span></Col>
         </Row>
+        <hr />
+        <br />
       </div>
     );
   }
