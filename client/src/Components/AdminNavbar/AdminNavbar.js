28119a1725ed612fdd8330ba9fbb573bbf2f1e37
Suvansh Shukla <suvanshshukla@gmail.com>
2021-05-09 11:50:16 +0530

    minor changes to adminNavbar

--- client/src/Components/AdminNavbar/AdminNavbar.js
+++ client/src/Components/AdminNavbar/AdminNavbar.js
@@ -25,6 +25,8 @@ const adminNavbar = (props) => {
           {/* ^^^ you have to edit this dropdown properly later */}
           <NavDropdown title={x} style={{ color: "white" }}>
             <NavDropdown.Item href="#action/3.1">Account Info</NavDropdown.Item>
+            <NavDropdown.Item href="/JobProfileForm">Add New Job Profile</NavDropdown.Item>
+            <NavDropdown.Item href="/Updates">Add New Update</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/Logout">Logout</NavDropdown.Item>
           </NavDropdown>
